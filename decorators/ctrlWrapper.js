1e8ecc9052d3579aea0a2ab153d7a967cc8d85f7
Djoult <55453354+Djoult@users.noreply.github.com>
2023-08-22 14:38:43 +0300

    adding decorators

---
+++ decorators/ctrlWrapper.js
@@ -0,0 +1,13 @@
+const ctrlWrapper = (ctrl) => {
+  const wrappedCtrl = async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  return wrappedCtrl;
+};
+
+export default ctrlWrapper;
