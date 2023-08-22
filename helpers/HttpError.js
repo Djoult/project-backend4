8044426cf58b63437f94e7a2b6a6d48d42a7f9f8
Djoult <55453354+Djoult@users.noreply.github.com>
2023-08-22 14:38:56 +0300

    adding helpers

---
+++ helpers/HttpError.js
@@ -0,0 +1,17 @@
+const defaultErrorMessages = {
+  400: "Bad request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not found",
+  409: "Conflict",
+};
+
+const HttpError = (status, message = defaultErrorMessages[status]) => {
+  const error = new Error(message);
+
+  error.status = status;
+
+  return error;
+};
+
+export default HttpError;
