c41f22f452f66339192bb10062cc47229d45b582
Djoult <55453354+Djoult@users.noreply.github.com>
2023-08-29 13:52:38 +0300

    changing address

--- controllers/auth/singUp.js
+++ controllers/auth/singUp.js
@@ -1,14 +1,14 @@
-import { User } from "../../models/index.js";
-import { HttpError, sendEmail } from "../../helpers/index.js";
-import bcrypt from "bcryptjs";
-import "dotenv/config";
-import { nanoid } from "nanoid";
+import { User } from '../../models/index.js';
+import { HttpError, sendEmail } from '../../helpers/index.js';
+import bcrypt from 'bcryptjs';
+import 'dotenv/config';
+import { nanoid } from 'nanoid';
 
 const singUp = async (req, res) => {
   const { name, email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
-    throw HttpError(409, "Email in use");
+    throw HttpError(409, 'Email in use');
   }
 
   const verificationToken = nanoid();
@@ -19,19 +19,19 @@ const singUp = async (req, res) => {
     password: hashPassword,
     verificationToken,
   });
-  const { PORT } = process.env;
+  const { BASE_URL } = process.env;
 
   const verifyEmail = {
     to: email,
-    subject: "Verify Email",
-    html: `<a href="http://localhost:${PORT}/api/auth/verify/${verificationToken}" target="_blank">Click verify email</a>`,
+    subject: 'Verify Email',
+    html: `<a href="${BASE_URL}/api/auth/verify/${verificationToken}" target="_blank">Click verify email</a>`,
   };
 
   await sendEmail(verifyEmail).then(() => {
     return res.status(201).json({
       name: newUser.name,
       email: newUser.email,
-      needVerification: true
+      needVerification: true,
     });
   });
 };
