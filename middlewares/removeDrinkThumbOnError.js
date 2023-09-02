a33231c00ec90f5053e068fd666258997a59dce3
dusixx <113414935+dusixx@users.noreply.github.com>
2023-09-02 22:42:01 +0300

    small fixes

--- middlewares/removeDrinkThumbOnError.js
+++ middlewares/removeDrinkThumbOnError.js
@@ -2,15 +2,13 @@ import fs from 'fs/promises';
 import { CLOUDINARY_THUMBS_DRINK_DIR } from '../constants/index.js';
 import { cloud } from '../helpers/index.js';
 
-export const removeDrinkThumbOnError = async req => {
-  const {
-    file,
-    body: { drinkThumb },
-  } = req;
+export const removeDrinkThumbOnError = async ({ file, body }) => {
+  const { drinkThumb } = body;
 
+  // удаляем картинку из tmp
+  // траим на случай если файл уже был удален в processDrinkThumb
   if (file) {
     try {
-      // траим на случай если файл был удален в processDrinkThumb
       await fs.unlink(file.newPath || file.path);
     } catch {}
   }
