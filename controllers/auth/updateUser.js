523b3a2bb2e7b73921e67bb0b29073201f0f9b06
Uliana Atanasova <kuv25ild@gmail.com>
2023-09-03 15:30:41 +0300

    added functional for changing user profile

--- controllers/auth/updateUser.js
+++ controllers/auth/updateUser.js
@@ -5,32 +5,39 @@ import fs from 'fs';
 
 const updateUser = async (req, res) => {
   const { _id: owner } = req.user;
-  const { name } = req.body;
-  const { filename } = req.file;
+  const { name } = JSON.parse(req.body?.json || {});
+  const { filename } = req.file || {};
+
   if (filename) {
-    const avatarUrl = path.join('/public', '/avatars', `/${filename}`);
+    const avatarUrl = path.join('avatars', filename)?.replace(/\\/g, '/');
 
     Jimp.read(path.join('tmp', filename), async (err, file) => {
       if (err) {
         throw err;
       }
- 
-      file.resize(100, 100).write(avatarUrl);
-      await fs.unlink(`tmp/${filename}`, (err) => {
+
+      file.resize(100, 100).write('public/' + avatarUrl);
+      await fs.unlink(`tmp/${filename}`, err => {
         if (err) throw err;
       });
-      const result = await User.findByIdAndUpdate(
+      const user = await User.findByIdAndUpdate(
         owner,
         { avatarUrl, name },
         { new: true }
       );
-      res.status(201).json(result);
+      res
+        .status(201)
+        .json({
+          email: user.email,
+          name: user.name,
+          avatarUrl: user.avatarUrl,
+        });
     });
   } else {
     const user = await User.findByIdAndUpdate(owner, { name }, { new: true });
     res
       .status(201)
-      .json({ email: user.email, name: user.name, avatarUrl: path.normalize(user.avatarUrl) });
+      .json({ email: user.email, name: user.name, avatarUrl: user.avatarUrl });
   }
 };
 
