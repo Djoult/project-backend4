a78040589fb5eb0e9f93fc2a01d63b787ae2b967
Djoult <55453354+Djoult@users.noreply.github.com>
2023-09-07 15:26:27 +0300

    changing email

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -5,8 +5,7 @@ import 'dotenv/config';
 import { nanoid } from 'nanoid';
 import { PASSWORD_MAX, PASSWORD_MIN } from '../../constants/misc.js';
 
-const 
-signUp = async (req, res) => {
+const signUp = async (req, res) => {
   console.log('signUp');
   const { name, email, password } = req.body;
 
@@ -30,7 +29,10 @@ signUp = async (req, res) => {
   const verifyEmail = {
     to: email,
     subject: 'Verify Email',
-    html: `<a href="${BASE_URL}/api/auth/verify/${verificationToken}" target="_blank">Click verify email</a>`,
+    html: `
+<div style ="border:0; background: black; width: 100%; display:block;outline:none;text-decoration:none;font-size:24px; color: white; padding-top: 6px; padding-left:1px;">
+If you have not registered with Drink Master, ignore this letter. If you want to verify your account, follow <a style ="color: orange" href="${BASE_URL}/api/auth/verify/${verificationToken}" target="_blank">the link</a></div>
+<img alt="Drink Master" height="auto" src="https://resizing.flixster.com/lLn5KKVLPHuCDOmZ-0UjoN6MuRo=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvMWEwNGRiMTQtODRkMi00ZjE3LWEzZDctNjY1ZGE1OGQyNTg1LmpwZw==" style="border:0;display:inline;outline:none;text-decoration:none;height:auto;width:100%;font-size:15px" class="CToWUd" data-bit="iit">`,
   };
 
   await sendEmail(verifyEmail).then(() => {
